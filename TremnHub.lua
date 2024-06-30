c59bee92cf96b000835a36fb40702e3b3af1e807
TremnDevelopment <juliagirlhehe@gmail.com>
2024-06-29 21:32:59 -1000

    Added Murder Mystery 2 game support

--- TremnHub.lua
+++ TremnHub.lua
@@ -1,9 +1,10 @@
 local MainLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/dirt",true))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/insanedude59/notiflib/main/main"))()
 
-Notification:Notification("Tremn Hub","Loading Tremn Hub...","GothamSemibold","Gotham",3)
+local Players = game:GetService('Players')
+local Player = Players.LocalPlayer
 
-local ConfigTable = {}
+Notification:Notification("Tremn Hub","Loading Tremn Hub...","GothamSemibold","Gotham",3)
 
 local InformationWindow = MainLib:CreateWindow("Information")
 InformationWindow:Section("Updated 6/29/2024")
@@ -17,5 +18,54 @@ SupportedWindow:Button("Information",function()
     print("Tremn Hub was still doesn't have any fighting games that are supported.")
 end)
 SupportedWindow:Section("Shooting Games")
+SupportedWindow:Button("Murder Mystery 2",function()
+    local MurderMysteryLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/dirt",true))()
+
+    local ConfigTable = {}
+
+    local MurderMysteryWindow = MurderMysteryLib:CreateWindow("Main")
+    MurderMysteryWindow:Section("Main")
+    MurderMysteryWindow:Toggle("Auto Grab Gun", {location = ConfigTable, flag = "Toggle"} , function()
+        -- Gun [Inside backpack or character], Knife [Inside backpack or character], and GunDrop [Inside workspace] --
+        local currentPosition = nil
+        while wait(.5) do
+            if not ConfigTable['Toggle'] then return end
+            for _, part in pairs(game.workspace:GetChildren()) do
+                if part.Name == 'GunDrop' then
+                    currentPosition = Player.Character.HumanoidRootPart.Position
+                    task.wait()
+                    for i = 1, 5 do
+                        Player.Character.HumanoidRootPart.Position = CFrame.new(part.Position)
+                        FireTouchInterest(game.Workspace:FindFirstChild('GunDrop'))
+                    end
+                    if Player.Character:FindFirstChild('Gun') or Player.Backpack:FindFirstChild('Gun') then
+                        for i = 1, 5 do
+                            Player.Character.HumanoidRootPart.Position = CFrame.new(currentPosition)
+                        end
+                    end
+                end
+            end
+            currentPosition = nil
+        end
+    end)
+
+    MurderMysteryWindow:Bind("Get Roles",{location = ConfigTable, flag = "KeyBind", default = Enum.KeyCode.B},function()
+        if ConfigTable['KeyBind'] then
+            for index, player in pairs(game:GetService("Players"):GetPlayers()) do
+                wait(1)
+                if player.Character:FindFirstChild('Gun') or player.Backpack:FindFirstChild('Gun') then
+                    warn(player.Name.. " is the sheriff of this round.")
+                    Notification:Notification("Roles", player.Name.." is the sherrif!","GothamSemibold","Gotham",.5)
+                elseif player.Character:FindFirstChild('Knife') or player.Backpack:FindFirstChild('Knife') then
+                    warn(player.Name.. " is the murderer of this round.")
+                    Notification:Notification("Roles", player.Name.." is the murderer!","GothamSemibold","Gotham",1)
+                else
+                    print(player.Name.. " is an innocent this round.")
+                    Notification:Notification("Roles", player.Name.." is innocent!","GothamSemibold","Gotham",1)
+                end
+            end
+        end
+    end)
+end)
 
 Notification:Notification("Tremn Hub","Loaded!","GothamSemibold","Gotham",3)
